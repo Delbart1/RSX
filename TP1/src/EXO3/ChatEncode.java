be3816dfaa1bb78094318a6e217a034f9f692223
Delbart Jonathan <delbart@a10p16.fil.univ-lille1.fr>
2017-01-12 17:49:52 +0100

    Final TP1

--- TP1/src/EXO3/ChatEncode.java
+++ TP1/src/EXO3/ChatEncode.java
@@ -4,17 +4,19 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 import java.util.Scanner;
 
 public class ChatEncode extends Thread {
 
 	private String myName;
 	private int port = 7654;
+	private final static String userName = "Jonathan";
 	private static String group = "224.0.0.2";
-	private final static int encodeKey = 5;
-	private final static int decodeKey = 5;
-	
+
 	public static void main(String[] args) throws IOException {
 
 		int port = 7654;
@@ -30,20 +32,20 @@ public class ChatEncode extends Thread {
 				while (true) {
 					byte buf[] = new byte[1024];
 					Scanner scanner = new Scanner(System.in);
-					String message = scanner.nextLine();
-					encode(message, encodeKey);
-					buf = message.getBytes();
+
+					String encodage;
+
+					encodage = getKey() + ":" + userName + ":" + encode(scanner.nextLine(), getKey());
+					buf = encodage.getBytes();
 					DatagramPacket pack = null;
 					try {
 						pack = new DatagramPacket(buf, buf.length, InetAddress.getByName(group), port);
 					} catch (UnknownHostException e1) {
-						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
 					try {
 						s.send(pack);
 					} catch (IOException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				}
@@ -57,14 +59,13 @@ public class ChatEncode extends Thread {
 
 			@Override
 			public void run() {
-
+				System.out.println("Welcome !");
 				while (true) {
 					MulticastSocket s = null;
 					String userID;
 					try {
 						s = new MulticastSocket(port);
 						s.joinGroup(InetAddress.getByName(group));
-
 
 						while (true) {
 
@@ -73,9 +74,11 @@ public class ChatEncode extends Thread {
 
 							s.receive(pack);
 							userID = pack.getAddress().getHostName();
-							
-							String receiveMess = new String(pack.getData(),0,pack.getLength());
-							System.out.println(userID + " : " + decode(receiveMess,decodeKey));
+
+							String receiveMess = new String(pack.getData(), 0, pack.getLength());
+							String[] tabMess = receiveMess.split(":");
+
+							System.out.println(tabMess[1] + " : " + decode(tabMess[2], Integer.parseInt(tabMess[0])));
 						}
 
 					} catch (IOException e) {
@@ -91,22 +94,54 @@ public class ChatEncode extends Thread {
 
 	}
 
-
-	
-	
-	public static String encode(String message, int shift){
+	public static String encode(String message, int shift) {
 		String res = "";
-		for(int  i = 0; i<message.length() ;i++){
-			res += (char)(int) (message.charAt(i) + shift);
+		for (int i = 0; i < message.length(); i++) {
+			res += (char) (int) (message.charAt(i) + shift);
 		}
 		return res;
 	}
-	
-	public static String decode(String message, int shift){
+
+	public static String decode(String message, int shift) {
 		String res = "";
-		for(int  i = 0; i<message.length() ;i++){
-			res += (char)(int) (message.charAt(i) - shift);
+		for (int i = 0; i < message.length(); i++) {
+			res += (char) (int) (message.charAt(i) - shift);
 		}
 		return res;
+	}
+
+	public static int getKey() {
+		String mac = "";
+		byte[] macAddress = null;
+
+		try {
+			InetAddress address = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		Enumeration<NetworkInterface> network = null;
+		try {
+			network = NetworkInterface.getNetworkInterfaces();
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+
+		while (network.hasMoreElements()) {
+			NetworkInterface net = network.nextElement();
+			try {
+				macAddress = net.getHardwareAddress();
+			} catch (SocketException e) {
+				e.printStackTrace();
+			}
+			if (macAddress != null) {
+				mac = "";
+				for (int i = 0; i < macAddress.length; i++) {
+					mac += String.format("%02X%s", macAddress[i], (i < macAddress.length - 1) ? "-" : "");
+				}
+			}
+		}
+		String[] macTab = mac.split("-");
+		return Integer.parseInt(macTab[macTab.length - 1], 16);
+
 	}
 }
